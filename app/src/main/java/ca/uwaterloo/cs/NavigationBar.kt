a9b96a59d0a0bd6f778bb56d8a8da8e102a42711
andyjianggg <andyjianggg1@gmail.com>
2022-06-27 18:57:27 -0400

    add new icons to navbar

--- app/src/main/java/ca/uwaterloo/cs/NavigationBar.kt
+++ app/src/main/java/ca/uwaterloo/cs/NavigationBar.kt
@@ -3,19 +3,20 @@ package ca.uwaterloo.cs
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import ca.uwaterloo.cs.ui.theme.InstagramPurple
 
 sealed class NavItem(
     var title: String,
     var icon: Int,
 ) {
-    object Catalogue: NavItem("Catalogue", R.drawable.ic_pumpkin)
-    object Logistics: NavItem("Logistics", R.drawable.ic_pumpkin)
-    object History: NavItem("History", R.drawable.ic_pumpkin)
-    object Profile: NavItem("Profile", R.drawable.ic_pumpkin)
+    // https://www.flaticon.com/free-icons
+    object Catalogue: NavItem("Catalogue", R.drawable.catalogue_icon)
+    object Logistics: NavItem("Logistics", R.drawable.logistics_icon)
+    object History: NavItem("History", R.drawable.history_icon)
+    object Profile: NavItem("Profile", R.drawable.profile_icon)
 }
 
 
@@ -29,13 +30,13 @@ fun NavigationBar() {
     )
     
     BottomNavigation(
-        //backgroundColor = Color.Blue
+        backgroundColor = Color.InstagramPurple
     ) {
         items.forEach { item ->
             BottomNavigationItem(
                 selected = false,
                 icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
-                label = { Text(text = item.title) },
+                //label = { Text(text = item.title) },
                 onClick = {
                     /*TODO*/
                 }
