69550096a1877d1397632a233b7a723f4eb66ec0
MarioZhang-Foxquilt <77451043+MarioZhang-Foxquilt@users.noreply.github.com>
2022-07-13 12:46:20 -0400

    Profile page added
    Profile page created + lined to navibar

--- app/src/main/java/ca/uwaterloo/cs/ui/theme/Theme.kt
+++ app/src/main/java/ca/uwaterloo/cs/ui/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 
 @Composable
 fun OnlineFoodRetailTheme(content: @Composable () -> Unit) {
@@ -19,8 +20,9 @@ fun OnlineFoodRetailTheme(content: @Composable () -> Unit) {
   val lightColors = lightColors(
     primary = Color.White,
     onPrimary = Color.DarkGray,
-    surface = Color.DarkGray,
+    surface = Color.DarkGray
   )
+
   MaterialTheme(
     colors = if (isSystemInDarkTheme()) darkColors else lightColors,
     content = {
@@ -34,6 +36,9 @@ fun OnlineFoodRetailTheme(content: @Composable () -> Unit) {
   )
 }
 
+val Color.Companion.TextPrimary get() = Color(0xFF191919)
+val Color.Companion.Spacer get() = Color(0xFFEDEDED)
+val Color.Companion.Divider get() = Color (0xFFE5E5E5)
 val Color.Companion.VeryDarkGray get() = Color(0xFF121212)
 val Color.Companion.InstagramOrange get() = Color(0xFFFF7A00)
 val Color.Companion.InstagramPeach get() = Color(0xFFFF0069)
