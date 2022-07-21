a1ccbc11ae559b057e49dd8392ca315fde9e8e12
Nikita Kabir <n6kabir@uwaterloo.ca>
2022-07-21 19:49:29 -0400

    simulate transaction logic working

--- app/src/main/java/ca/uwaterloo/cs/simulateTransaction/SimulateTransaction.kt
+++ app/src/main/java/ca/uwaterloo/cs/simulateTransaction/SimulateTransaction.kt
@@ -1,32 +1,27 @@
-package ca.uwaterloo.cs.product
+package ca.uwaterloo.cs.simulateTransaction
 
+import androidx.annotation.IntegerRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
-import ca.uwaterloo.cs.destinations.MainContentDestination
 import ca.uwaterloo.cs.destinations.ProductFormDestination
 import ca.uwaterloo.cs.form.FormState
 import ca.uwaterloo.cs.platform.PlatformState
+import ca.uwaterloo.cs.product.ProductInformation
 import ca.uwaterloo.cs.ui.theme.OnlineFoodRetailTheme
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.lang.NumberFormatException
 
 @Destination
 @Composable
@@ -39,6 +34,7 @@ fun simulateTransaction(
     val formState by remember { mutableStateOf(FormState()) }
     val platformState by remember { mutableStateOf(PlatformState(data)) }
     var reductionAmount by remember { mutableStateOf("") }
+    var reductionAmountIsError by remember { mutableStateOf(false) }
 
 
     OnlineFoodRetailTheme {
@@ -67,21 +63,42 @@ fun simulateTransaction(
                 value = reductionAmount,
                 onValueChange = { reductionAmount = it },
                 label = { Text("Reduction Amount") },
+                isError = reductionAmountIsError,
                 modifier = androidx.compose.ui.Modifier.align(Alignment.CenterHorizontally),
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
             )
 
             Button(
                 onClick = {
-                nav.navigate(ProductFormDestination(data))
+                    reductionAmountIsError = updateProductAfterTransactionIsError(data, reductionAmount)
+                    if (!reductionAmountIsError) {
+                        nav.navigate(ProductFormDestination(data))
+                    }
                 },
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             ) {
-//            eventually this will call back end to reduce product amount by reduction amount
-//            for now, return to product form
                 Text(text = "Confirm Simulate Transaction")
             }
         }
 
     }
+}
+
+private fun updateProductAfterTransactionIsError(data: ProductInformation, reductionAmountInput: String): Boolean
+{
+    var reductionAmount = 0
+    try {
+        reductionAmount = reductionAmountInput.toInt()
+    }
+    catch (e: NumberFormatException) {
+        return true
+    }
+    if (data.platform1) {
+        data.platform1_amount -= reductionAmount
+    }
+    if (data.platform2) {
+        data.platform2_amount -= reductionAmount
+    }
+    data.amount -= reductionAmount
+    return false
 }
